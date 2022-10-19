b88e2fa754c07d9b4f5593cba762d08df9e937e0
Bitcoin Supex <admin@bitcoinsupex.com>
2022-10-19 11:48:51 -0300

    updated
    updated

---
+++ src/lang.js
@@ -0,0 +1,151 @@
+window.lang={
+
+    tv:["Tired of checking price?",
+    "Bored of copying addresses?",
+    "Do not wait more!",
+    "Bitcoin Supex To The Rescue!!!",
+    "Up to Three Coins Program",
+    "Set Effective Alarms",
+    "Create QR Codes with a Click",
+    "Associate Addresses With Aliases",
+    "Quick Search in One Click"],
+    text_M:[
+        "YOUR EXTENSION",
+        "COMING SOON",
+        "ALSO",
+        "FOR ANDROID",
+        "& IPHONE"
+        ],
+
+    text_runner:['BITCOINSUPEX','with bs you can:','* set alarms',
+    '* create qr','* real time price', '* address book',
+    'all that and more', 'what are you waiting for!', 'install it now!'
+    ],
+
+    tv_text:[
+        "<span style='font-family:supex;font-size:24px;color:green'>FINANCIAL FREEDOM GROUP PRESENTS</span>",
+        "<span style='font-family:supex;font-size:24px;color:gold'>THE FIRST CHROME EXTENSION APP</span>",
+        "<span style='font-family:supex;font-size:24px;color:orange'>FOR BITCOINS LOVERS AND TRADERS</span>",
+        `CHOOSE UP TO THREE LOCAL CURRENCIES`,
+        "<span style='font-size:14px;color:green;font-family:supex'>VIEW THE PRICE ON EXCHANGERS WHILE YOU BROWSE</span>",
+        "ALWAYS SEE THE PRICE IN REAL TIME",
+        "CREATE CUSTOMIZABLE ALARMS",
+        "CREATE FREQUENT ADDRESS BOOK",
+        "CREATE QR CODES EASILY",
+        "SELECT BETWEEN 15 DIFFERENT LANGUAGES",
+        "<span style='font-family:supex;font-size:24px;color:red'>ALL THIS AND MUCH MORE</span>",
+        "<span style='font-family:supex;font-size:24px;color:purple'>WITH THE PROFESSIONAL TRADERS TOOL</span>",
+        "<span style='font-family:supex;font-size:24px;color:cian'>DOWNLOAD IT NOW FROM THE CHROME WEB STORE</span>",
+    
+    ],
+
+    tab_title:["BITCOIN SUPEX ULTIMATE EDITION ",
+    "SEE LOCAL PRICES ",
+    "SET LOCAL ALARM ",
+    "QR MAKER ",
+    "ADDRESS BOOK",
+    "AND MUCH MORE",
+    "* * SUPER EXTENSION * * * ",
+    ],
+
+    promo:`
+    * Receive Promotions, * Contest for Gift Cards, * Get Discount Coupons
+     <br> * Find out new and amazing apps in the crypto universe that we will publish <br>
+     * To be aware of all this and more! leave us your contact email
+  `,
+  button_promo:'Sign up and Participate',
+
+ desc_title_1:'Local Alarm',
+ desc_desc_1:'What is the most important tool a trader has? Information. Without the information no trader who calls himself respectable can do much because it is this that gives meaning to trading. If they could inform you when the price goes down or up with respect to your local currency that you previously choose, you would be at an advantage.' ,
+ desc_title_2:'Real Time Price',
+ desc_desc_2:'Imagine being able to see in real time the price of bitcoin for the last 15 seconds just by looking next to the url of your browser while you are browsing. Now imagine being able to see the same thing in real time but up to 3 currencies of your choice and the best thing!: You can choose any currency in the world',
+ desc_title_3:'Notebook',
+ desc_desc_3:`Would you like to be able to save frequently used addresses and associate them with specific aliases? .
+ If you frequently use your own addresses from different wallets or from third parties, this option is perfect for you!
+ Not only can you associate addresses to aliases and save them locally in the browser to be able to paste them whenever you like, but you can also choose to paste them anywhere by right clicking on the field you want to paste the address without having to copy it to your clipboard or you can also include it to your clipboard. Isn't it great?`,
+ desc_title_4:'Create QR',
+ desc_desc_4:'Convert your bitcoin addresses, web pages and any text in general into QR codes with just one click',
+
+ patner:'Sponsors',
+
+ opinion_1:'After installing the extension in my browser I never had to worry about knowing the price in real time again, it is really very useful',
+ opinion_2:'I was looking for exactly something like this, it really exceeded my expectations simply the best tool of its kind',
+ opinion_3:'I never thought I could find a tool like this, it\'s just what I needed now I can keep an eye on it without even looking at the price',
+
+
+ about:'About BitcoinSupex',
+ about_desc:'We developed Bitcoin Supex with the intention that everyone could enjoy a real-time experience of the price of the bit and we basically listened to all the opinions of people who were tired of not finding a tool that suits their requirements and wrote in reviews and posts. . of similar tools in the chrome shop. We firmly believe in the money revolution and that each one of us can be their own bank, that\'s why we develop everything that has to do with helping the crypto sector.',
+
+ users:'Users',
+ lang:'Languages',
+ emp:'Full Time Employees',
+ down:'Download now',
+
+ copy:'All rights reserved',
+ install:'INSTALL',
+ btn:'Download for Chrome'
+
+}
+
+
+
+
+/*
+
+window.lang={
+
+    tv:["Cansado de revisar precio?",
+    "Aburrido de copiar direcciones?",
+    "No Espere Mas!",
+    "Bitcoin Supex Al Rescate!!!",
+    "Programa Hasta Tres Monedas",
+    "Configura Alarmas Eficacez",
+    "Crea Codigos QR con un Clic",
+    "Asocia Direcciones Con Alias",
+    "Busqueda Rapida en un Click"],
+    
+    text_M:[
+        "TU EXTENSION",
+        "PROXIMAMENTE",
+        "TAMBIEN",
+        "PARA ANDROID",
+        "& IPHONE"
+        ],
+
+    text_runner:['BITCOINSUPEX','con bs puedes:','* configura alarmas',
+    '* crear qr','* precio real time','* libreta de direcciones',
+    'todo eso y mas','que esperas!','instalalo ya!'
+    ],
+
+    tv_text:[
+        "<span style='font-family:supex;font-size:24px;color:green'>FINANCIAL FREEDOM GROUP PRESENTS</span>",
+        "<span style='font-family:supex;font-size:24px;color:gold'>LA PRIMERA CHROME EXTENSION APP</span>",
+        "<span style='font-family:supex;font-size:24px;color:orange'>PARA AMANTES DE BITCOINS Y TRADERS</span>",
+        `ELIGE HASTA TRES MONEDAS LOCALES `,
+        "<span style='font-size:14px;color:green;font-family:supex'>MIRA EL PRECIO EN EXCHANGERS MIENTRAS NAVEGAS</span>",
+        "MIRA EL PRECIO EN TIEMPO REAL SIEMPRE",
+        "CREA ALARMAS PERSONALIZABLES",
+        "CREA LIBRO DE DIRECCIONES FRECUENTES",
+        "CREA CODIGOS QR FACILMENTE",
+        "SELECCIONA ENTRE 15 LENGUAJES DIFERENTES",
+        "<span style='font-family:supex;font-size:24px;color:red'>TODO ESTO Y MUCHO MAS</span>",
+        "<span style='font-family:supex;font-size:24px;color:purple'>CON LA HERRAMIENTA DE TRADERS PROFESIONALES</span>",
+        "<span style='font-family:supex;font-size:24px;color:cian'>DESCARGALA AHORA MISMO EN CHROME WEB STORE</span>",
+    
+    ],
+
+    tab_title:["BITCOIN SUPEX ULTIMATE EDITION ",
+    "SEE LOCAL PRICES     ",
+    "SET LOCAL ALARM      ",
+    "QR MAKER  ",
+    "ADDRESS BOOK  ",
+    "AND MUCH MORE  ",
+    "* * SUPER EXTENSION * * *    ",
+    ]
+
+
+
+}
+
+
+*/
